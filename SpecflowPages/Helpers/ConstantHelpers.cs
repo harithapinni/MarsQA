74d1ae00175d3da9724c8be0660a0eb1b10616ae
Haritha <30430970+harithapinni@users.noreply.github.com>
2021-09-28 03:45:42 +1300

    Seller Profile details
    Seller Profile details

---
+++ SpecflowPages/Helpers/ConstantHelpers.cs
@@ -0,0 +1,42 @@
+﻿using RelevantCodes.ExtentReports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsQA_1.Helpers
+{
+  
+    public class ConstantHelpers
+    {
+        
+        //Base Url
+        public static string Url = "http://localhost:5000";
+
+        //ScreenshotPath
+        //public static string ScreenshotPath = "";
+        public static string ScreenshotPath = getCodeDirectory() + @"\TestReports\Screenshots\";
+        //ExtentReportsPath
+       //public static string ReportsPath = "";
+        public static string ReportsPath = getCodeDirectory() + @"\TestReports\Report.html";
+        //ReportXML Path
+        //public static string ReportXMLPath = "";
+        public static string ReportXMLPath = getCodeDirectory() + @"\TestReports\XML";
+        public static string getCodeDirectory()
+        {
+            var filepath = System.AppDomain.CurrentDomain.BaseDirectory;
+            return filepath + @"..\..\";
+            
+            
+        }
+
+
+
+
+
+    }
+
+
+
+}
